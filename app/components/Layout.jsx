4de1c2cc0f77016aa2e9143f30fbf061b389bea7
pete04h3 <peetmj@gmail.com>
2023-03-29 10:05:17 +0200

    added cart

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,15 +1,32 @@
 import {Drawer, useDrawer} from '~/components/Drawer';
 import {Suspense, useEffect} from 'react';
-import {Await, useMatches} from '@remix-run/react';
+import {Await, useMatches, useFetchers} from '@remix-run/react';
+import {CartLineItems, CartActions, CartSummary} from '~/components/Cart';
 
 export function Layout({children, title}) {
   const {isOpen, openDrawer, closeDrawer} = useDrawer();
+  const fetchers = useFetchers();
   const [root] = useMatches();
   const cart = root.data?.cart;
   // Test opening the drawer, delete this after verifying
   // useEffect(() => {
   //   openDrawer();
   // }, []);
+
+  // Grab all the fetchers that are adding to cart
+  const addToCartFetchers = [];
+  for (const fetcher of fetchers) {
+    if (fetcher?.submission?.formData?.get('cartAction') === 'ADD_TO_CART') {
+      addToCartFetchers.push(fetcher);
+    }
+  }
+
+  // When the fetchers array changes, open the drawer if there is an add to cart action
+  useEffect(() => {
+    if (isOpen || addToCartFetchers.length === 0) return;
+    openDrawer();
+  }, [addToCartFetchers]);
+
   return (
     <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
       <header
@@ -30,8 +47,9 @@ export function Layout({children, title}) {
       >
         {children}
       </main>
+
       <Drawer open={isOpen} onClose={closeDrawer}>
-        <h2>TODO Cart Data</h2>
+        <CartDrawer cart={cart} close={closeDrawer} />
       </Drawer>
     </div>
   );
@@ -64,6 +82,44 @@ function CartHeader({cart, openDrawer}) {
               </div>
             )}
           </button>
+        )}
+      </Await>
+    </Suspense>
+  );
+}
+
+function CartDrawer({cart, close}) {
+  return (
+    <Suspense>
+      <Await resolve={cart}>
+        {(data) => (
+          <>
+            {data?.totalQuantity > 0 ? (
+              <>
+                <div className="flex-1 overflow-y-auto">
+                  <div className="flex flex-col space-y-7 justify-between items-center md:py-8 md:px-12 px-4 py-6">
+                    <CartLineItems linesObj={data.lines} />
+                  </div>
+                </div>
+                <div className="w-full md:px-12 px-4 py-6 space-y-6 border border-1 border-gray-00">
+                  <CartSummary cost={data.cost} />
+                  <CartActions checkoutUrl={data.checkoutUrl} />
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+                <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+                  Your cart is empty
+                </h2>
+                <button
+                  onClick={close}
+                  className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+                >
+                  Continue shopping
+                </button>
+              </div>
+            )}
+          </>
         )}
       </Await>
     </Suspense>
